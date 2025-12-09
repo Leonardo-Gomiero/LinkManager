eea254f74c608b5a3ba7ef3420c1af50eb9642c9
Leonardo Capoccia <leoleonardo2000@gmail.com>
2025-12-09 09:21:39 -0300

    feat: adds Open API documentation (Swagger)

--- LinkManager.api/Program.cs
+++ LinkManager.api/Program.cs
@@ -9,6 +9,10 @@ builder.Services.AddInfrastructureServices(builder.Configuration);
 // Register authentication & authorization
 builder.Services.AddJwtAuthentication(builder.Configuration);
 
+// Register Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 app.UseAuthentication();
@@ -19,5 +23,12 @@ app.MapGet("/", () => "LinkManager API is running securely!");
 // Map endpoints from separate modules
 app.MapUserEndpoints();
 app.MapLinkEndpoints();
+
+// Enable Swagger middleware in development
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
 
 app.Run();
